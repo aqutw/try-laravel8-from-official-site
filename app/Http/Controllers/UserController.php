337a236ca6493fb68d7071b415d3743b62d4050e
aqutw <aquarian.ex@gmail.com>
2020-10-25 17:46:02 +0800

    trying controllers - explicit use middleware in controller code INSTEAD OF routes/web.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,22 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        echo <<<EOF
+        \$this->middleware('auth');
+
+        \$this->middleware('log')->only('index');
+
+        \$this->middleware('subscribed')->except('store');
+        EOF;
+    }
+
     public function index()
     {
         echo 'user index.';
