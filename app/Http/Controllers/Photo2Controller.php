28d0232b58e105bf8ee3dbd140e757a9abf9b59d
aqutw <aquarian.ex@gmail.com>
2020-10-25 17:46:37 +0800

    php artisan make:controller Photo2Controller --resource --model=Photo2

---
+++ app/Http/Controllers/Photo2Controller.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Photo2;
+use Illuminate\Http\Request;
+
+class Photo2Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Photo2  $photo2
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Photo2 $photo2)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Photo2  $photo2
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Photo2 $photo2)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Photo2  $photo2
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Photo2 $photo2)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Photo2  $photo2
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Photo2 $photo2)
+    {
+        //
+    }
+}
